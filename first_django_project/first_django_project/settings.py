1868324952ef49af98c84796b412d14f10a90baf
Liezel <liezellalay@gmail.com>
2025-09-17 21:08:58 +0800

    Fix INSTALLED_APPS, remove todo

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -31,15 +31,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    # ... default apps ...
-    'TrickIt', # Add your app here
-    'todo',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    'TrickIt',  # Your custom app
 ]
 
 MIDDLEWARE = [
