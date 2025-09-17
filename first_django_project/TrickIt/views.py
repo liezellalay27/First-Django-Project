368394592d552ff6175b4da60cda9ceda4a3d7e9
Liezel <liezellalay@gmail.com>
2025-09-18 05:23:13 +0800

    Added task views and updated rendering

--- first_django_project/TrickIt/views.py
+++ first_django_project/TrickIt/views.py
@@ -4,13 +4,12 @@ def home(request):
     return render(request, 'home.html')
 
 def task_list(request):
-    # Your logic for displaying a list of tasks
-    return render(request, 'home.html') # Or whatever template you want to use for the task list
+    tasks = Task.objects.all()
+    context = {'tasks': tasks}
+    return render(request, 'tasks.html', context)
 
 def update_task(request, pk):
-    # Your logic for updating a task
-    return render(request, 'home.html') # This should be a template for updating a task
+    return render(request, 'tasks.html')
 
 def delete_task(request, pk):
-    # Your logic for deleting a task
-    return render(request, 'home.html') # This should be a confirmation page for deleting a task
+    return render(request, 'tasks.html')
