e0172f7434065ded31266a7f1e8aa21948d5f666
Liezel <liezellalay@gmail.com>
2025-09-17 22:09:59 +0800

    Added missing view functions (task_list, update_task, delete_task)

--- first_django_project/TrickIt/views.py
+++ first_django_project/TrickIt/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 
+def home(request):
+    return render(request, 'home.html')
+
 def task_list(request):
-    # Your view logic goes here
-    # Render the existing home.html template
-    return render(request, 'TrickIt/home.html')
+    # Your logic for displaying a list of tasks
+    return render(request, 'home.html') # Or whatever template you want to use for the task list
+
+def update_task(request, pk):
+    # Your logic for updating a task
+    return render(request, 'home.html') # This should be a template for updating a task
+
+def delete_task(request, pk):
+    # Your logic for deleting a task
+    return render(request, 'home.html') # This should be a confirmation page for deleting a task
