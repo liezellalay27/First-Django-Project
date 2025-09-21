7b27d7e996c8d92ee9438c667a584f5224a6c698
Liezel <liezellalay@gmail.com>
2025-09-21 21:07:44 +0800

    Updated Django app configuration in settings.py

--- first_django_project/first_django_project/settings.py
+++ first_django_project/first_django_project/settings.py
@@ -37,8 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
-    'TrickIt',  # Your custom app
+    'TrickIt.apps.TrickitConfig', # Add this line
 ]
 
 MIDDLEWARE = [
