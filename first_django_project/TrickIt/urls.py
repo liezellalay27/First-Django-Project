a06047edc200eac8da28889ea93f3e3f2a0f9ba0
Liezel <liezellalay@gmail.com>
2025-09-21 20:51:25 +0800

    Updated home.html and register.html for navigation

--- first_django_project/TrickIt/urls.py
+++ first_django_project/TrickIt/urls.py
@@ -1,10 +1,11 @@
+# TrickIt/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('tasks/', views.task_list, name='task-list'),
-    path('update-task/<int:pk>/', views.update_task, name='update-task'),
-    path('delete-task/<int:pk>/', views.delete_task, name='delete-task'),
     path('register/', views.register, name='register'),
+    path('tasks/update/<int:pk>/', views.update_task, name='update-task'),
+    path('tasks/delete/<int:pk>/', views.delete_task, name='delete-task'),
 ]
